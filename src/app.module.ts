5634e531a14c42d5244e9116da588f041a24ca49
OanceaAlex <alexoancea99@gmail.com>
2023-08-18 13:12:46 +0300

    sets up .env and config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+
 
 @Module({
-  imports: [AuthenticationModule],
+  imports: [
+    AuthenticationModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      })
+    }),
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }) 
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
