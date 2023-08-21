69e39ce03440d23de734811385114d060c22f32c
OanceaAlex <alexoancea99@gmail.com>
2023-08-21 12:42:35 +0300

    removes magic strings from login route

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginUserDto } from 'src/users/dtos/login-user.dto';
+import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req, @Body() loginUserDto: LoginUserDto) {
     return req.user;
